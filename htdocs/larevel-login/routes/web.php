3635400556db6068bdc280f303591c3dcda1d1d6
kenjikuroki <90098303+kenjikuroki@users.noreply.github.com>
2022-01-02 11:59:31 +0900

    logout

--- htdocs/larevel-login/routes/web.php
+++ htdocs/larevel-login/routes/web.php
@@ -12,13 +12,19 @@ use App\Http\Controllers\Auth\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::middleware(['guest'])->group(function () {
-    Route::get('/', [AuthController::class,'showLogin'])->name('showLogin');
-    Route::post('login',[AuthController::class, 'login'])->name('login'); 
+Route::group(['middleware' => ['guest']], function () {
+  // ログインフォーム表示
+  Route::get('/', [AuthController::class, 'showLogin'])->name('login.show');
+  // ログイン処理
+  Route::post('login', [AuthController::class, 'login'])->name('login');
 });
 
-Route::middleware(['auth'])->group(function () {
-Route::get('home', function() {
+Route::group(['middleware' => ['auth']], function () {
+  // ホーム画面
+  Route::get('home', function() {
     return view('home');
-  })->name('resources\views\home');
+  })->name('home');
+  // ログアウト
+  Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 });
+
